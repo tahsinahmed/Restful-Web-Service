4ef324d7600949f1ffdf4fead52b6ecf5bd87237
TAHSINahmed <st.ahmed14@gmail.com>
2023-12-26 00:54:35 +0600

    intial commit

---
+++ src/main/java/com/tahsin/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.tahsin.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello Spring.");
+    }
+}
