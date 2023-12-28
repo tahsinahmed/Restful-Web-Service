73059126e020f617b3ae23777db1241d53ddcdce
tahsinahmed <st.ahmed14@gmail.com>
2023-12-28 15:35:25 +0600

    Customize rest api response with static and dynamic filtering

--- src/main/java/com/tahsin/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/tahsin/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.tahsin.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -7,8 +8,10 @@ import java.time.LocalDate;
 
 public class User {
     private Integer id;
+    @JsonProperty("user_name")
     @Size(min = 2, message = "Name should have atleast 2 characters")
     private String name;
+    @JsonProperty("birth_date")
     @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
 
