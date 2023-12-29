38929ac4e78046d8d9e9e4eaab7a5541676a3f47
tahsinahmed <st.ahmed14@gmail.com>
2023-12-29 19:14:07 +0600

    configured spring security

---
+++ src/main/java/com/tahsin/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.tahsin.rest.webservices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().fullyAuthenticated());
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
